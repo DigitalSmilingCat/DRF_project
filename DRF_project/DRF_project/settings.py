52def8a02f7691e787d40419ef002d5c0dd03df5
DigitalSmilingCat <avdonin.oleg@gmail.com>
2023-07-03 23:11:58 +0300

    Change DB to PostgreSQL

--- DRF_project/DRF_project/settings.py
+++ DRF_project/DRF_project/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'DRF_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drf',
+        'USER': os.getenv('DRF_DB_LOGIN'),
+        'PASSWORD': os.getenv('DRF_DB_PASSWORD'),
+        'HOST': os.getenv('DRF_DB_HOST'),
+        'PORT': os.getenv('DRF_DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
